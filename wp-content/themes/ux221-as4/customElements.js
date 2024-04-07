76d7087a795100661bc5d0caa44c47d29dda33bb
Rathesa <Rathesa@users.noreply.github.com>
2024-04-07 16:46:16 +0000

    deploy: f0104b2758564b6e3e63cbcb74150a1e338b7729

--- wp-content/themes/ux221-as4/customElements.js
+++ wp-content/themes/ux221-as4/customElements.js
@@ -7,7 +7,7 @@ class CopyrightYear extends HTMLElement{
 customElements.define("x-year", CopyrightYear);
 class TwoSidedMarket extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<a href="sewing-guide">Sewing Guide</a>&nbsp; <a href="products">Products</a>`;
+    this.innerHTML = `<a href="sewing-guides">Sewing Guides</a>&nbsp; <a href="products">Products</a>`;
   }
 }
 customElements.define("x-twosided", TwoSidedMarket);
